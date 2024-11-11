5aaf2526c8f42d31e9a06200952f5a225426e733
smallphan <smallphan.cn@gmail.com>
2024-11-11 11:15:01 +0800

    Bug fixed:     - 注册失败
    Update:
        - 增加前端代码 register 逻辑
        - Json Web Token 增加 expiration timestamp

--- src/table/users.rs
+++ src/table/users.rs
@@ -1,6 +1,8 @@
 use actix_web::{ get, post, web::{ self, Bytes, Json }, Error, HttpRequest, HttpResponse };
-use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{ decode, encode, Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation };
 use sqlx::PgPool;
+
 
 pub struct Users {
     id: i64,
@@ -16,17 +18,18 @@ struct LoginReq {
     password_hash: String
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Debug, serde::Deserialize)]
 struct RegisterReq {
     username: String,
     password_hash: String,
     role: i8,
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct Claims {
     id: String,
-    role: i8
+    role: i8,
+    exp: u64
 }
 
 #[post("/login")]
@@ -54,9 +57,16 @@ pub async fn Login(
         return Err(actix_web::error::ErrorUnauthorized("Password not found."));
     }
 
+    // Token expiration timestamp
+    let expiration = Utc::now()
+        .checked_add_signed(Duration::minutes(30))
+        .expect("Valid timestamp")
+        .timestamp() as u64;
+
     let claims: Claims = Claims {
         id: user.id.to_string(),
         role: user.role.unwrap_or_default() as i8, 
+        exp: expiration
     };
 
     let key = 
@@ -84,22 +94,31 @@ pub async fn Register(
     request: HttpRequest
 ) -> Result<HttpResponse, Error> {
 
+    println!("Register request reveived. {:?}", regInfo);
+    println!("Request: {:?}", request);
+
     let claims = UnwrapToken(&request)?;
+
+    println!("Unwrap success: {:?}", claims);
 
     // Check if user role is Administrator
     if claims.role != '0' as i8 {
         return Err(actix_web::error::ErrorUnauthorized("Authorization invalid."));
     } 
 
+    println!("Administrator validation success!");
+
     // Insert a new user
-    sqlx::query!("INSERT INTO \"user\" (id, username, password_hash, role) VALUES ($1, $2, $3, $4)", 
-        6, &regInfo.username, &regInfo.password_hash, &regInfo.role) // TODO: Fix id as primary key.
+    sqlx::query!("INSERT INTO \"user\" (username, password_hash, role) VALUES ($1, $2, $3)", 
+        &regInfo.username, &regInfo.password_hash, &regInfo.role) 
         .execute(pool.get_ref())
         .await
         .map_err(|err| {
             println!("Database error: {:?}", err);
             actix_web::error::ErrorForbidden("Insert failed.")
         })?;
+
+    println!("Insert success!");
 
     Ok(HttpResponse::Ok().body("Register success."))
 }
@@ -112,18 +131,23 @@ fn UnwrapToken(
         std::env::var("ENCODING_KEY")
             .expect("Encoding Key undefined.");
 
-    let mut validation = Validation::new(Algorithm::HS256);
-    validation.validate_exp = false;
+    let mut validation = Validation::default();
 
     // Decode and unwrap for claims in token
     if let Some(authorHeader) = request.headers().get("Authorization") {
+        println!("request.headers().get(\"Authorization\")");
         if let Ok(authorString) = authorHeader.to_str() {
+            println!("authorHeader.to_str()");
             if let Some(token) = authorString.strip_prefix("Bearer ") {
-                return match decode::<Claims>(
+                println!("authorString.strip_prefix(\"Bearer \")");
+                println!("Token: {}", token);
+                let decode = decode::<Claims>(
                     token,
                     &DecodingKey::from_secret(key.as_ref()), 
                     &validation
-                ) {
+                );
+                println!("Decode: {:?}", decode);
+                return match decode {
                     Ok(data) => Ok(data.claims),
                     Err(_) => Err(actix_web::error::ErrorUnauthorized("Authorization invalid."))
                 };
